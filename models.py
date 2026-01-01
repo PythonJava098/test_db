8ae6e2dc50680187d1e2832b8e20b1d53b7ed5ca
PythonJava098 <161448814+PythonJava098@users.noreply.github.com>
2026-01-01 11:25:07 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
-from database import Base
 from sqlalchemy import Column, Integer, String, Float
+from database import Base
 
 class UrbanResource(Base):
     __tablename__ = "urban_resources"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
-    category = Column(String)  # e.g., "hospital", "atm", "school"
+    category = Column(String)  # hospital, bank, atm, petrol_pump
     latitude = Column(Float)
     longitude = Column(Float)
+    capacity = Column(Integer, default=50) # Scale 1-100 (100 = Major Hospital)
     address = Column(String, nullable=True)
